c00a8a1e1b26b360c1048cb1726ad757ee22df38
Piotr Plenik <piotr.plenik@gmail.com>
2017-07-28 10:24:20 +0200

    Implement validation

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -8,15 +8,17 @@ import play.api.data._
 import play.api.data.Forms._
 
 import scala.collection.mutable.ListBuffer
+import scala.concurrent.Future
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class HomeController @Inject()(mcc: MessagesControllerComponents, items: ItemList)
-  extends MessagesAbstractController(mcc) {
-
+class HomeController @Inject()(
+  mcc: MessagesControllerComponents,
+  items: ItemList
+) extends MessagesAbstractController(mcc) {
   val itemListForm: Form[Checkout] = Form(
     mapping(
       "items" -> list(
@@ -25,39 +27,56 @@ class HomeController @Inject()(mcc: MessagesControllerComponents, items: ItemLis
           "id" -> longNumber,
           "quantity" -> number
         )
-        // itemListForm -> CheckoutItem
-        ((id, quantity) => CheckoutItem(id, "", quantity))
-        // CheckoutItem -> itemListForm
-        ((ci: CheckoutItem) => Some(ci.id, ci.quantity))
+          // itemListForm -> CheckoutItem
+          ((id, quantity) => CheckoutItem(id, "", quantity))
+          // CheckoutItem -> itemListForm
+          ((ci: CheckoutItem) => Some(ci.id, ci.quantity))
+          verifying("Not enough in stock.", item => {
+            val stockQuantity = items.getItem(item.id) match {
+              case Some(i) => i.quantity
+              case None => 0
+            }
+
+            item.quantity <= stockQuantity
+          })
       )
     )
-    ((items) => { Checkout(items.to[ListBuffer]) })
+    ((items) => {
+      Checkout(items.to[ListBuffer])
+    })
     ((c: Checkout) => Some(c.items.toList))
   )
 
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index() = Action { implicit request: MessagesRequest[AnyContent] =>
     val checkout = new Checkout(items)
 
     Ok(views.html.index(items, itemListForm.fill(checkout)))
   }
 
-  def submit() = Action { implicit request: MessagesRequest[AnyContent] =>
+  def submit() = Action.async { implicit request: MessagesRequest[AnyContent] =>
     itemListForm.bindFromRequest.fold(
       formWithErrors => {
         println("Errors: " + formWithErrors)
-        BadRequest(views.html.index(items, formWithErrors))
+        Future.successful(
+          BadRequest(views.html.index(items, formWithErrors))
+        )
       },
       checkout => {
         checkout.items.map(i => items.takeOff(i.id, i.quantity))
 
-        Ok(views.html.submit(items, checkout))
+        Future.successful(
+          Ok(views.html.submit(
+            items,
+            checkout
+          ))
+        )
       }
     )
   }
